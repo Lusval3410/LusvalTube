92fb885efff2f2307618a8eb88ffec0c98e18bad
Lusval3410 <elfafara93@gmail.com>
2026-07-22 21:17:50 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+function buscar(){
+
+
+let texto =
+document.getElementById("busqueda").value;
+
+
+
+document.getElementById("resultados").innerHTML =
+
+`
+
+<div class="video">
+
+<h2>Buscaste:</h2>
+
+<p>${texto}</p>
+
+<p>Motor de YouTube próximamente 🗿</p>
+
+
+</div>
+
+`;
+
+}
